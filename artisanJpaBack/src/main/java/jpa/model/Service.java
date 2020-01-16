ebf5035021c8b0ed4cf8b5af07d89aabe91d5fb3
Nico <touboulnicolas@gmail.com>
2020-01-16 16:59:57 +0100

    nico test

--- artisanJpaBack/src/main/java/jpa/model/Service.java
+++ artisanJpaBack/src/main/java/jpa/model/Service.java
@@ -15,67 +15,12 @@ public class Service {
 	private String Metier;
 
 
-<<<<<<< HEAD
-public long getIdService() {
-	return idService;
-}
-
-
-
-public void setIdService(long idService) {
-	this.idService = idService;
-}
-
-
-
-public Metier getMetier() {
-	return Metier;
-}
-
-
-
-public void setMetier(Metier metier) {
-	Metier = metier;
-}
-
-
-
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((Metier == null) ? 0 : Metier.hashCode());
-	result = prime * result + (int) (idService ^ (idService >>> 32));
-	return result;
-}
-
-
-
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Service other = (Service) obj;
-	if (Metier == null) {
-		if (other.Metier != null)
-			return false;
-	} else if (!Metier.equals(other.Metier))
-		return false;
-	if (idService != other.idService)
-		return false;
-	return true;
-}
-
 
 
 public Service() {
 	super();
 }
-=======
+
 	public long getIdService() {
 		return idService;
 	}
@@ -95,10 +40,6 @@ public Service() {
 		Metier = metier;
 	}
 
-
-	public Service() {
-		super();
-	}
 
 
 	@Override
@@ -129,6 +70,6 @@ public Service() {
 			return false;
 		return true;
 	}
->>>>>>> nicolas
+
 
 }
