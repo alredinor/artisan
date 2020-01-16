faefcfea2d1065429d7635a879414ad4aa7071fb
charley <charleybossard@gmail.com>
2020-01-16 15:20:50 +0100

    Changement JpaContext

--- artisanJpaBack/src/main/java/jpa/dao/DaoMetierJpaImpl.java
+++ artisanJpaBack/src/main/java/jpa/dao/DaoMetierJpaImpl.java
@@ -2,43 +2,55 @@ package jpa.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import jpa.model.Metier;
+import jpa.util.JpaContext;
 
 public class DaoMetierJpaImpl implements DaoMetier
 {
 
 	@Override
-	public Metier findBykey(Long key) {
+	public Metier findBykey(Long key) 
+	{
+		EntityManager em = JpaContext.getInstance().createEntityManager();	
+		Metier p = null;
+		p = em.find(Metier.class, key);
+		em.close();
+		return p;
+	}
+
+	@Override
+	public List<Metier> findAll() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Metier> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void insert(Metier obj) {
+	public void insert(Metier obj) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Metier update(Metier obj) {
+	public Metier update(Metier obj)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void delete(Metier obj) {
+	public void delete(Metier obj) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void deleteByKey(Long key) {
+	public void deleteByKey(Long key) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
