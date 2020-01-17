65e9199af4d3c533a66abba03f949b13e1a0443a
Hubert MOKET <hubertayiwa@gmail.com>
2020-01-17 12:46:27 +0100

    test hubert

--- artisanJpaBack/src/test/java/jpa/TestHubert.java
+++ artisanJpaBack/src/test/java/jpa/TestHubert.java
@@ -1,7 +1,17 @@
 package jpa;
 
 import java.util.Scanner;
+
+import jpa.dao.DaoCompte;
+import jpa.dao.DaoCompteFactory;
 import jpa.dao.DaoCompteJpaImpl;
+import jpa.dao.DaoService;
+import jpa.dao.DaoServiceFactory;
+import jpa.model.Adresse;
+import jpa.model.Artisan;
+import jpa.model.Compte;
+import jpa.model.Service;
+import jpa.util.JpaContext;
 
 public class TestHubert {
 	
@@ -40,7 +50,33 @@ public class TestHubert {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bonjour Hubert");
+		JpaContext.getInstance();
+		DaoCompte daoCompte=DaoCompteFactory.getInstance();
+		DaoService daoService=DaoServiceFactory.getInstance();
+		
+		Service s = new Service();
+		Service s2 = new Service();
+		Service s3 = new Service();
+		
+		s.setNomService("Plomberie");
+		s2.setNomService("Menuiserie");
+		s3.setNomService("Electricite");
+		
+		daoService.insert(s);
+		daoService.insert(s2);
+		daoService.insert(s3);
+		
+		Compte c= new Artisan();
+		c.setLogin("toto");
+		c.setMdp("3007");
+		c.setEmail("toto@nico.fr");
+		Adresse a= new Adresse();
+		a.setNumero(1);
+		a.setCodePostal("91");
+		a.setRue("ici");
+		a.setVille("Paris");
+		c.setAdresse(a);
+		daoCompte.insert(c);
 
 	}
 
