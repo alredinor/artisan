c911f375f7f648901a0aaef9d02367d61db16d64
alban <alban.delouvee@gmail.com>
2020-01-17 15:28:41 +0100

    alban

--- artisanJpaBack/src/test/java/jpa/TestAlban.java
+++ artisanJpaBack/src/test/java/jpa/TestAlban.java
@@ -1,7 +1,16 @@
 package jpa;
 
+import jpa.dao.DaoCompte;
+import jpa.dao.DaoCompteFactory;
+import jpa.dao.DaoService;
+import jpa.dao.DaoServiceFactory;
+import jpa.util.JpaContext;
+
 public class TestAlban {
 	public static void main(String[] args) {
-		
+		JpaContext.getInstance();
+		DaoCompte daoCompte=DaoCompteFactory.getInstance();
+		DaoService daoService=DaoServiceFactory.getInstance();
+		JpaContext.destroy();
 	}
 }
